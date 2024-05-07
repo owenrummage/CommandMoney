ca4b4a4d792432cc98a0c9a8a0bc5b23a9afd2e2
Owen Rummage <owen@rummage.cc>
2024-05-07 00:47:47 -0500

    done?

--- src/data.go
+++ src/data.go
@@ -4,27 +4,30 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path"
 )
 
 var dataPath = ""
 var decoded Wallet
+var walletName = ".config/wallet.json"
+var walletMode = 0755
 var initalWallet = `{
-	"id": 0,
+	"id": "0",
 	"name": "Default Wallet",
 	"currentBalance": 0,
 	"transactions": []
 }`
 
 type Transaction struct {
-	ID     int    `json:"id"`
+	ID     string `json:"id"`
 	Amount int    `json:"amount"`
 	Reason string `json:"reason"`
 }
 
 type Wallet struct {
-	ID             int           `json:"id"`
+	ID             string        `json:"id"`
 	Name           string        `json:"name"`
 	CurrentBalance int           `json:"currentBalance"`
 	Transactions   []Transaction `json:"transactions"`
@@ -37,7 +40,7 @@ func InitDatastore() {
 		panic("Error getting user home directory")
 	}
 
-	dataPath = path.Join(dir, ".config/wallet.json")
+	dataPath = path.Join(dir, walletName)
 
 	data, err := os.ReadFile(dataPath)
 	if errors.Is(err, os.ErrNotExist) {
@@ -57,15 +60,43 @@ func decodeData(data []byte) {
 		panic("JSON Parse Error: " + err.Error())
 	}
 }
+func encodeData() []byte {
+	var enc, err = json.Marshal(decoded)
+	if err != nil {
+		panic("JSON Encoding Error: " + err.Error())
+	}
+	return enc
+
+}
 func createWallet() {
-	var err = os.WriteFile(dataPath, []byte(initalWallet), 0755)
+	var err = os.WriteFile(dataPath, []byte(initalWallet), fs.FileMode(walletMode))
 	if err != nil {
 		panic("Unable to write to data file: " + err.Error())
 	}
 }
 
-func getAllTransactions() {
-	for index, element := range decoded.Transactions {
-		fmt.Printf("Transaction %d: {id: %d, amount: %d, reason: %s}\r\n", index, element.ID, element.Amount, element.Reason)
+func writeData(data []byte) {
+	var dir, err = os.UserHomeDir()
+
+	if err != nil {
+		panic("Error getting user home directory")
 	}
+
+	dataPath = path.Join(dir, walletName)
+
+	err = os.WriteFile(dataPath, data, fs.FileMode(walletMode))
+	if err != nil {
+		panic("Unable to write data file: " + err.Error())
+	}
+	decodeData(data)
+}
+
+func addTransaction(transaction Transaction) {
+	decoded.Transactions = append(decoded.Transactions, transaction)
+	var encoded = encodeData()
+	writeData(encoded)
+}
+
+func getAllTransactions() []Transaction {
+	return decoded.Transactions
 }
--- src/main.go
+++ src/main.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strconv"
 
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
 
 func main() {
 
 	var cmdLog = &cobra.Command{
-		Use:   "log [amount] [reason]",
-		Short: "Log money coming in or out of the account",
-		Long:  `Logs the money coming in or out of your account with a reason listed.`,
-		Args:  cobra.RangeArgs(2, 2),
+		Use:                "log [operation a,s] [amount] [reason]",
+		Short:              "Log money coming in or out of the account",
+		Long:               `Logs the money coming in or out of your account with a reason listed.`,
+		Args:               cobra.RangeArgs(2, 2),
+		DisableFlagParsing: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Logged Transaction: '" + args[1] + "' for amount " + args[0] + " at time " + time.Now().Format("2006-01-02 15:04:05"))
-			// todo save transactions
+			var tempTrans Transaction
+			tempTrans.ID = uuid.NewString()
+
+			amountInt, err := strconv.Atoi(args[0])
+			if err != nil {
+				// ... handle error
+				panic(err)
+			}
+			tempTrans.Amount = amountInt
+
+			tempTrans.Reason = args[1]
+
+			addTransaction(tempTrans)
 		},
 	}
 
@@ -24,20 +37,60 @@ func main() {
 		Use:   "info",
 		Short: "Show all information about the wallet.",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Wallet Information")
-			// todo print information
+			transactions := getAllTransactions()
+			transTotal := len(transactions)
+
+			var transDeposit int
+			var transWithdrawl int
+			var totalBalance int
+			var transactionList string
+
+			for index, element := range transactions {
+				_ = index
+				if element.Amount > 0 {
+					transDeposit = transDeposit + 1
+				}
+				if element.Amount < 0 {
+					transWithdrawl = transWithdrawl + 1
+				}
+				totalBalance = totalBalance + element.Amount
+
+				transactionList += fmt.Sprintf("  {id: %s, amount: %d, reason: %s}\r\n", element.ID, element.Amount, element.Reason)
+
+			}
+
+			fmt.Printf(`##################
+  COMMAND WALLET
+##################
+
+
+Stats
+-------
+  Total Transactions: %d
+  Total Deposits: %d
+  Total Withdrawls: %d
+		  
+  Total Balance: $%d
+		  
+Transactions
+-------------
+%s`, transTotal, transDeposit, transWithdrawl, totalBalance, transactionList)
 		},
 	}
-	var cmdTest = &cobra.Command{
-		Use:   "test",
-		Short: "balls",
+	var cmdList = &cobra.Command{
+		Use:   "list",
+		Short: "List transactions",
 		Run: func(cmd *cobra.Command, args []string) {
-			getAllTransactions()
+			transactions := getAllTransactions()
+
+			for index, element := range transactions {
+				fmt.Printf("Transaction %d: {id: %s, amount: %d, reason: %s}\r\n", index, element.ID, element.Amount, element.Reason)
+			}
 		},
 	}
 
 	var rootCmd = &cobra.Command{Use: "money"}
-	rootCmd.AddCommand(cmdLog, cmdInfo, cmdTest)
+	rootCmd.AddCommand(cmdLog, cmdInfo, cmdList)
 	InitDatastore()
 	rootCmd.Execute()
 }
